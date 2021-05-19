11fca9a8edac6df0cc33ce9d81df54fb97e728bd
Jedidiah <nosteeeeeee@gmail.com>
2021-05-19 13:11:10 +0800

    made the environment unique on each project

--- 1. Image Processing/main.py
+++ 1. Image Processing/main.py
@@ -1,1 +1,6 @@
-print("hey")
+from PIL import Image, ImageFilter
+
+img = Image.open("./Pokedex/4.4 pikachu.jpg")
+
+filtered_image = img.filter(ImageFilter.BLUR)
+filtered_image.save("blur.png","png")
